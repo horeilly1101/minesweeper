39b3dd25ddc560689adaa8dd04da38d7dc5e8e3d
Hugh O'Reilly <horeilly1101@gmail.com>
2020-08-19 15:25:51 -0500

    update reducer tests

--- client/src/reducers/updateGameState.test.js
+++ client/src/reducers/updateGameState.test.js
@@ -5,12 +5,10 @@ import { revealSquare, flagSquare } from "../actions";
 const TEST_STATE_1 = {
     bombSquares: [3],
     areBombSquaresInitialized: true,
-    squares: {
-        0: {count: null, status: SQUARE_STATUS.HIDDEN},
-        1: {count: null, status: SQUARE_STATUS.HIDDEN},
-        2: {count: null, status: SQUARE_STATUS.HIDDEN},
-        3: {count: null, status: SQUARE_STATUS.HIDDEN}
-    },
+    squares: Object.assign({}, Array(2 * 2).fill({
+        status: SQUARE_STATUS.HIDDEN,
+        count: null,
+    })),
     numBombsFlagged: 0,
     numSquaresCleared: 0,
     gameStatus: GAME_STATUS.IN_PROGRESS,
@@ -19,19 +17,52 @@ const TEST_STATE_1 = {
     numCols: 2,
 };
 
-test("test 2x2 board", () => {
-    // Clear the squares that don't have bombs.
-    const afterClick0 = updateGameState(TEST_STATE_1, revealSquare(0));
-    expect(afterClick0.squares[0].status).toBe(SQUARE_STATUS.CLEARED);
-    expect(afterClick0.squares[0].count).toBe(1);
-    const afterClick1 = updateGameState(afterClick0, revealSquare(1));
-    expect(afterClick1.squares[1].status).toBe(SQUARE_STATUS.CLEARED);
-    expect(afterClick1.squares[1].count).toBe(1);
-    const afterClick2 = updateGameState(afterClick1, revealSquare(2));
-    expect(afterClick2.squares[2].status).toBe(SQUARE_STATUS.CLEARED);
-    expect(afterClick2.squares[2].count).toBe(1);
-    // Flag the only bomb.
-    const afterFlag = updateGameState(afterClick2, flagSquare(3));
-    expect(afterFlag.squares[3].status).toBe(SQUARE_STATUS.FLAGGED);
-    expect(afterFlag.gameStatus).toBe(GAME_STATUS.WON);
+describe("test 2x2 board", () => {
+    test("win the game", () => {
+        // Clear the squares that don't have bombs.
+        const afterClick0 = updateGameState(TEST_STATE_1, revealSquare(0));
+        expect(afterClick0.squares[0].status).toBe(SQUARE_STATUS.CLEARED);
+        expect(afterClick0.squares[0].count).toBe(1);
+        const afterClick1 = updateGameState(afterClick0, revealSquare(1));
+        expect(afterClick1.squares[1].status).toBe(SQUARE_STATUS.CLEARED);
+        expect(afterClick1.squares[1].count).toBe(1);
+        const afterClick2 = updateGameState(afterClick1, revealSquare(2));
+        expect(afterClick2.squares[2].status).toBe(SQUARE_STATUS.CLEARED);
+        expect(afterClick2.squares[2].count).toBe(1);
+        // Flag the only bomb.
+        const afterFlag = updateGameState(afterClick2, flagSquare(3));
+        expect(afterFlag.squares[3].status).toBe(SQUARE_STATUS.FLAGGED);
+        expect(afterFlag.gameStatus).toBe(GAME_STATUS.WON);
+    });
+
+    test("click on the bomb", () => {
+        expect(updateGameState(TEST_STATE_1, revealSquare(3)).gameStatus)
+            .toBe(GAME_STATUS.LOST);
+    });
+});
+
+const TEST_STATE_2 = {
+    bombSquares: [2, 8],
+    areBombSquaresInitialized: true,
+    squares: Object.assign({}, Array(3 * 3).fill({
+        status: SQUARE_STATUS.HIDDEN,
+        count: null,
+    })),
+    numBombsFlagged: 0,
+    numSquaresCleared: 0,
+    gameStatus: GAME_STATUS.IN_PROGRESS,
+    numBombs: 2,
+    numRows: 3,
+    numCols: 3,
+};
+
+test("test 3x3 board", () => {
+    const actions = [
+        revealSquare(3), revealSquare(5), flagSquare(2),
+        flagSquare(8),
+    ];
+    const finalState = actions.reduce(
+        (state, action) => updateGameState(state, action), TEST_STATE_2
+    );
+    expect(finalState.gameStatus).toBe(GAME_STATUS.WON);
 });
