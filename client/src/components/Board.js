66150f483806c12970916d72b4fdd83f716cee7a
Hugh O'Reilly <horeilly1101@gmail.com>
2020-08-19 15:47:13 -0500

    move some constants to be more configurable

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from "react-redux";
 import Square from './Square';
 import { rightClickSquare, clickSquare } from "../actions";
-import { NUM_ROWS, NUM_COLS } from "../constants";
 
 
 class Board extends React.Component {
@@ -23,8 +22,9 @@ class Board extends React.Component {
     }
 
     renderRow(row) {
-        const squares = Array(NUM_COLS).fill(null)
-            .map((val, i) => this.renderSquare(NUM_COLS * row + i));
+        const numCols = this.props.numCols;
+        const squares = Array(numCols).fill(null)
+            .map((val, i) => this.renderSquare(numCols * row + i));
         return (
             <div className="board-row" key={row}>
                 {squares}
@@ -33,7 +33,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const rows = Array(NUM_ROWS).fill(null)
+        const rows = Array(this.props.numRows).fill(null)
             .map((val, i) => this.renderRow(i));
         return (
             <div>
@@ -45,6 +45,8 @@ class Board extends React.Component {
 
 const mapStateToProps = state => ({
     squares: state.squares,
+    numRows: state.numRows,
+    numCols: state.numCols,
 });
 
 const mapDispatchToProps = dispatch => ({
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,3 @@
-export const NUM_ROWS = 20;
-export const NUM_COLS = 30;
-export const NUM_BOMBS = 80;
-export const BOARD_SIZE = NUM_ROWS * NUM_COLS;
 export const SQUARE_STATUS = {
     HIDDEN: 0,
     CLEARED: 1,
